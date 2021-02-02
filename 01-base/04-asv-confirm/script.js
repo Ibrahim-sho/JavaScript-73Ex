c7708ec7c57e649114a59c3d9c5cf9e2f377c9c8
Ibra <ibrahimshoaib838@gmail.com>
2021-02-02 14:55:22 +0100

    01 done

---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function questions() {
+
+    // your code here
+    let age = parseInt(prompt("How old are you ?"));
+    let gender = prompt("What is your gender?");
+    let city = prompt("In which city do you live?");
+
+    let answer = confirm("Do you confirm your answers ?" + "\n" + age + "\n" + gender +"\n"+ city);
+        if (answer == true) {
+            alert("Thanks for the information");
+        }
+        else{
+            questions();
+        }
+
+})();
